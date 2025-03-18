9105413912560caf6c65756d4b9485da81362d44
heeyoung-choi <oprocinante@gmail.com>
2025-03-18 16:20:47 +0700

    initial push

---
+++ Data/Migrations/20250318014743_custom_user_data.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class custom_user_data : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomTag",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomTag",
+                table: "AspNetUsers");
+        }
+    }
+}
